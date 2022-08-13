85f6a03da188760edbed5505f3241865be128de5
durdufatih <mfdurdu@gmail.com>
2022-08-13 12:57:36 +0300

    Api Key Has been deleted from Const file

--- app/src/main/java/com/fatihdurdu/manext/util/Const.kt
+++ app/src/main/java/com/fatihdurdu/manext/util/Const.kt
@@ -3,5 +3,5 @@ package com.fatihdurdu.manext.util
 object Const {
     const val BASE_URL = "https://api.unsplash.com"
     const val LIST_URL = "/photos"
-    const val API_KEY="NzNJ-7CtwVXqD-B6jdr4d-UJqxgQ5rLaXKmXGJ3xp9k"
+    const val API_KEY="[Your Api Key]"
 }
